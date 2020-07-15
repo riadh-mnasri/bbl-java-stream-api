741fffffc3f41ad7f3216288c31c1aa14410b596
Riadh MNASRI <riadh.mnasri@gmail.com>
2020-07-15 21:29:37 +0200

    Add demo base

---
+++ src/test/java/com/riadhmnasri/bbl/streamapi/BBLStreamApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riadhmnasri.bbl.streamapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BBLStreamApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
